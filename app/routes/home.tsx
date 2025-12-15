f543b89754e5150e69c8fa338f1b2d6b92c83b99
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2025-12-15 18:14:09 +0800

    feat: cleanup meta tags

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,29 +1,7 @@
-import type { MetaDescriptor } from "react-router";
-import type { Route } from "./+types/home";
-
-export function meta({}: Route.MetaArgs): MetaDescriptor[] {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-    { name: "og:title", content: "New React Router App" },
-    {
-      name: "og:description",
-      content: "Welcome to React Router!",
-    },
-    {
-      name: "twitter:title",
-      content: "New React Router App",
-    },
-    {
-      name: "twitter:description",
-      content: "Welcome to React Router!",
-    },
-  ];
-}
-
 export default function Home() {
   return (
     <main className="max-w-prose px-4 pt-4">
+      <title>Home</title>
       <h1 className="mb-2 text-2xl font-semibold tracking-tight">Home</h1>
       <p className="leading-relaxed">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic praesentium eum nostrum atque
